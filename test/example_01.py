8d19f4f68b2074aeb7565ef155b394bf5b345ba3
Niels Bassler <bassler@clin.au.dk>
2024-09-13 19:07:54 +0200

    more documentation

--- test/example_01.py
+++ test/example_01.py
@@ -16,12 +16,25 @@ def main(args=None):
     logging.basicConfig(level=logging.DEBUG)
 
     # Filenames and directories
+
+    # working directory with the input images and ROIs are located:
+    # test dataset 20230427_EBT.tar.gz will be provided somewhere in the future.
+    data_dir = "/home/bassler/Desktop/20230427_EBT/"
+
+    # first 4 scans of pre-irradiation images.
     pre_filenames = [
         "img20230424_13241513.tif",
         "img20230424_13243703.tif",
         "img20230424_13245545.tif",
         "img20230424_13251336.tif"
     ]
+
+    # in this case there are several regoings of interest in the image.
+    # the ROIs we marked in ImageJ, and then saved by ImageJ as a zip file.
+    # should it be a single ROI, then it has suffix .roi, which is also OK.
+    roi_pre_filename = "RoiSet_pre1.zip"
+
+    # post irradiation images, again 4 scans of the same area
     post_filenames = [
         "img20230427_11184159.tif",
         "img20230427_11190007.tif",
@@ -29,34 +42,39 @@ def main(args=None):
         "img20230427_11193478.tif"
     ]
 
-    roi_pre_filename = "RoiSet_pre1.zip"
+    # ROI size and positions do not have to match the pre-irradiation ROIs.
+    # However, the number of ROIs must match the number of ROIs in the pre-irradiation image.
     roi_post_filename = "RoiSet_post1.zip"
 
+    # Calibration file, which is a pickle file containing the calibration data.
+    # It is also possible to use the Calibration class to make a new set, but here we assume this is already done.
     calibration_file = "./resources/ebt_calibration_lot03172103_RED.pkl"
-    data_dir = "/home/bassler/Desktop/20230427_EBT/"
-    # test dataset 20230427_EBT.tar.gz will be provided somewhere
 
     # Load images
     pre_image = RSImage([os.path.join(data_dir, fn) for fn in pre_filenames])
     post_image = RSImage([os.path.join(data_dir, fn) for fn in post_filenames])
 
-    # Load ROIs and attach them to the images
+    # In case no ROIs were made with ImageJ, it can also directly be can also be set manually
+    # These are a list of tuples, for example for 3 ROIs:
+    # roi_pre.rois = [(50, 100, 50, 100), (150, 200, 150, 200), (250, 300, 250, 300)]
+    # Here, however, we load the ROIs from the ImageJ zip file:
     roi_pre = ROI(os.path.join(data_dir, roi_pre_filename))
     roi_post = ROI(os.path.join(data_dir, roi_post_filename))
-    # can also be set manually, if no ROI file is available
+
+    # and attach the ROI tuple lists to the images:
     pre_image.rois = roi_pre.rois
     post_image.rois = roi_post.rois
 
-    # Simple analysis by ROI
+    # Simple analysis by ROI, which means, each ROI an average dose is calculated:
     results_by_roi = analyze_simple_roi(
         pre_image, post_image, calibration_file)
     print(f"Results by ROI: {results_by_roi}")
 
-    # Simple analysis by 2D image
+    # But alternatively, we can also do a full image analysis, which means, the full post_image is converted from pixel_values to dose:
     results_by_image = analyze_simple_image(
         pre_image, post_image, calibration_file)
 
-    # Plot the full-image results and save it to file
+    # Plot the full-image dose map and save it to file
     plot_results(results_by_image, dpi=300, pixel_size=0.1,
                  save="dose_distribution.png")
 
